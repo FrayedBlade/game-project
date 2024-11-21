252f1bbfdb791fe6a2ae2bee89beeb941e7e57a3
FrayedBlade <email@yourdomain.com>
2024-11-21 11:46:22 +0100

    Gravity test 3 working!

--- src/game_project/core.clj
+++ src/game_project/core.clj
@@ -6,30 +6,21 @@
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+  (q/color-mode :hsb))
 
 (def player (atom {:x 100 :y 100}))
 
-(defn update-state [state]
-   ; Update sketch state by changing circle color and position.
-   {:color (mod (+ (:color state) 0.7) 255)
-    :angle (+ (:angle state) 0.1)}
+(defn update-state []
   (let [x (:x @player)
         y (:y @player)]
     (if (< x 400) (swap! player update :x + 5))))
 
 
 
-(defn draw-state [state]
+(defn draw-state []
   ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
+  (update-state)
 
   (let [x (:x @player)
         y (:y @player)]
@@ -37,17 +28,16 @@
 
 
 (q/defsketch game-project
-  :title "You spin my circle right round"
-  :size [500 500]
-  ; setup function called only once, during sketch initialization.
-  :setup setup
-  ; update-state is called on each iteration before draw-state.
-  :update update-state
-  :draw draw-state
-  :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
-  :middleware [m/fun-mode])
-
-;test
+             :title "You spin my circle right round"
+             :size [500 500]
+             ; setup function called only once, during sketch initialization.
+             :setup setup
+             ; update-state is called on each iteration before draw-state.
+             :update update-state
+             :draw draw-state
+             :features [:keep-on-top]
+             ; This sketch uses functional-mode middleware.
+             ; Check quil wiki for more info about middlewares and particularly
+             ; fun-mode.
+             ;:middleware [m/fun-mode]
+             )
