8e9b093a694125ef67eaeaec49a97522243d255c
FrayedBlade <email@yourdomain.com>
2024-11-21 13:05:12 +0100

    Text test (not working)

--- src/game_project/core.clj
+++ src/game_project/core.clj
@@ -2,19 +2,32 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(def screen-sizeX 1000)
+(def screen-sizeY 600)
+
+(def box-sizeX 50)
+(def box-sizeY 50)
+
 (defn setup []
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb))
 
-(def player (atom {:x 100 :y 100 :vx 0 :vy 0}))
+(def player (atom {:x (/ screen-sizeX 3) :y 100 :vx 0 :vy 0}))
+
+
 
 (defn update-state []
   (let [x (:x @player)
         y (:y @player)
         vy (:vy @player)]
-    (if (< y 400) (swap! player update :vy + 1) (do (swap! player assoc :y 400) (swap! player assoc :vy 0) ))
+    (if (> y (- screen-sizeY box-sizeY 1))
+      (do (swap! player assoc :vy 0)
+          (swap! player assoc :y (- screen-sizeY box-sizeY))
+          (q/text "Game over!" (/ screen-sizeX 2) (/ screen-sizeY 2))
+          )
+      (do (swap! player update :vy + 1)))
     (swap! player update :y + vy)
 
     (if (q/key-pressed?) (swap! player assoc :vy -10))
@@ -28,13 +41,19 @@
     (update-state)
 
     (let [x (:x @player)
+          y (:y @player)
+          vy (:vy @player)]
+      (if (> y (- screen-sizeY box-sizeY 1))
+        (q/text "Game over!" (/ screen-sizeX 2) (/ screen-sizeY 2))))
+
+    (let [x (:x @player)
           y (:y @player)]
-      (q/rect x y 100 100))))
+      (q/rect x y box-sizeX box-sizeY))))
 
 
 (q/defsketch game-project
              :title "You spin my circle right round"
-             :size [500 500]
+             :size [screen-sizeX screen-sizeY]
              ; setup function called only once, during sketch initialization.
              :setup setup
              ; update-state is called on each iteration before draw-state.
