5a130707a185f459eb0fe5427aebdd514aa11e32
FrayedBlade <email@yourdomain.com>
2024-11-21 09:28:54 +0100

    Drawing rect

--- src/game_project/core.clj
+++ src/game_project/core.clj
@@ -12,6 +12,8 @@
   {:color 0
    :angle 0})
 
+(def player (atom {:x 100 :y 100}))
+
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
   {:color (mod (+ (:color state) 0.7) 255)
@@ -23,14 +25,10 @@
   ; Set circle color.
   (q/fill (:color state) 255 255)
   ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+
+  (let [x (:x @player)
+        y (:y @player)]
+    (q/rect x y 100 100)))
 
 
 (q/defsketch game-project
