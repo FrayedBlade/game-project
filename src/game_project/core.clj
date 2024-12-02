c9d1609b6173691fe02a58f6c83c7486810a0da2
FrayedBlade <email@yourdomain.com>
2024-12-02 15:31:38 +0100

    Clojure bird code cleanup

--- src/game_project/core.clj
+++ src/game_project/core.clj
@@ -3,16 +3,22 @@
             [quil.middleware :as m]))
 
 (def screen-sizeX 1000)
-(def screen-sizeY 600)
+(def screen-sizeY 700)
 
 (def box-sizeX 50)
 (def box-sizeY 50)
+
+(def jump-strength 10)
 
 (def obstacle-gap 200)
 (def obstacle-density 200)
 (def obstacle-speed 4)
 
-(def obstacle-size screen-sizeX)
+(def obstacle-sizeX 30)
+(def obstacle-sizeY (max screen-sizeY screen-sizeX))
+
+(def player (atom {:x (/ screen-sizeX 3) :y (/ screen-sizeY 3) :vx 0 :vy 0}))
+(def obstacles (atom []))
 
 (defn setup []
   ; Set frame rate to 30 frames per second.
@@ -20,11 +26,7 @@
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb))
 
-(def player (atom {:x (/ screen-sizeX 3) :y 100 :vx 0 :vy 0}))
-(def obstacle (atom {:x (+ screen-sizeX box-sizeX) :y (/ screen-sizeY 2)}))
-(def obstacles (atom []))
 
-(def v [{:a 1 :b 2} {:c 3 :d 4} {:e 5 :f 6}])
 
 
 
@@ -48,61 +50,36 @@
           (q/text "Game over!" (/ screen-sizeX 2) (/ screen-sizeY 2)) ;text
           )
       (do (swap! player update :vy + 0.7)))                 ;Gravity Strength
-    (if (< y 0)
-      (do (swap! player assoc :vy 0)
-          (swap! player assoc :y 0)))
+
 
     (swap! player update :y + vy)                           ;player movement
 
     (if (and @key-pressed-trigger (q/key-pressed?))
       (do
-        (swap! player assoc :vy -7)                         ;jump
+        (swap! player assoc :vy (- 0 jump-strength))                         ;jump
         (reset! key-pressed-trigger false)))
     (if (and (not @key-pressed-trigger) (not (q/key-pressed?)))
       (do
         (reset! key-pressed-trigger true)))
     )
-  (let [x (:x @obstacle)
-        y (:y @obstacle)]
-    (if (not @game-over)
-      (swap! obstacle update :x - 2)))                      ;obstacle moving
+
+
+  ;(let [x (:x @obstacle)
+  ;      y (:y @obstacle)]
+  ;  (if (not @game-over)
+  ;    (swap! obstacle update :x - 2)))                      ;obstacle moving
 
 
 
-  (swap! obstacles #(mapv                                   ;obstacles moving
+  (if (not @game-over)
+    (swap! obstacles #(mapv                                   ;obstacles moving
                       (fn [param1]
-                        (update param1 :x - obstacle-speed)) %))
+                        (update param1 :x - obstacle-speed)) %)))
 
-  (for [[k v] @obstacles]
-    (if (not @game-over)
-      ;(swap! obstacleX update :x - 2 )
+  (if (and (game-over) ()))
 
-      (println (str k ": " v))
-      )
-    )
 
-  ;(let [x (:x @player)                                      ;Collision detection
-  ;      y (:y @player)
-  ;      ox (:x @obstacle)
-  ;      oy (:y @obstacle)]
-  ;  (if (or
-  ;        (and
-  ;          (> (+ x box-sizeX) ox)
-  ;          (> (+ y box-sizeY) oy)
-  ;          (< x (+ ox box-sizeY))
-  ;          )
-  ;        (and
-  ;          (> (+ x box-sizeX) ox)
-  ;          (< y (- oy 200))
-  ;          (< x (+ ox box-sizeY))))
-  ;    (do (q/fill 0)                                        ;set text color
-  ;        (q/text-size 100)                                 ;set text size
-  ;        (q/text-align :center :center)                    ;align text horizontal and vertical
-  ;        (q/text "Game over!" (/ screen-sizeX 2) (/ screen-sizeY 2)) ;text
-  ;        (swap! player assoc :vy 0)
-  ;        (swap! player assoc :vx 0)
-  ;        (reset! game-over true)
-  ;        )))
+
   (doseq [m @obstacles]
     (let [x (:x @player)                                      ;Collision detection
           y (:y @player)
@@ -112,12 +89,12 @@
             (and
               (> (+ x box-sizeX) ox)
               (> (+ y box-sizeY) oy)
-              (< x (+ ox box-sizeY))
+              (< x (+ ox obstacle-sizeX))
               )
             (and
               (> (+ x box-sizeX) ox)
               (< y (- oy obstacle-gap))
-              (< x (+ ox box-sizeY))))
+              (< x (+ ox obstacle-sizeX))))
         (do (q/fill 0)                                        ;set text color
             (q/text-size 100)                                 ;set text size
             (q/text-align :center :center)                    ;align text horizontal and vertical
@@ -131,20 +108,23 @@
 
 
 
-  (if (empty? @obstacles)
+  (if (empty? @obstacles)                                   ;spawning of obstacles
     (do (swap! obstacles conj {:x (+ screen-sizeX box-sizeX)
                                :y (random-range obstacle-gap
-                                                (- screen-sizeY obstacle-gap))})
+                                                screen-sizeY)})
         )
     (do (if (< (:x (last @obstacles)) (- screen-sizeX obstacle-density ))
           (do
             (swap! obstacles conj {:x (+ screen-sizeX box-sizeX)
                                    :y (random-range obstacle-gap
-                                                    (- screen-sizeY obstacle-gap))}) ;working
+                                                    screen-sizeY)})
             )
           )))
 
-  (swap! obstacles #(filter (fn [param1] (>= (:x param1) (- 0 box-sizeX))) %)) ;deletes the obstacle if x lover than {0 - box-sizeX}
+  (swap! obstacles
+         #(filter
+            (fn [param1]
+              (>= (:x param1) (- 0 box-sizeX))) %)) ;deletes the obstacle if x lover than {0 - box-sizeX}
 
 
 
@@ -159,23 +139,18 @@
   ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
   (update-state)
-  (let [x (:x @player)
+  (let [x (:x @player)                                      ;player drawing
         y (:y @player)]
     (q/fill 200)
     (q/rect x y box-sizeX box-sizeY))
-  ;(let [x (:x @obstacle)
-  ;      y (:y @obstacle)]
-  ;  (q/fill 100)
-  ;  (q/rect x (- y obstacle-size 200) box-sizeX obstacle-size)
-  ;  (q/rect x y box-sizeX obstacle-size))
 
 
-  (doseq [m @obstacles]
+  (doseq [m @obstacles]                                     ;obstacle drawing
     (let [x (:x m)
           y (:y m)]
       (q/fill 100)
-      (q/rect x (- y obstacle-size obstacle-gap) box-sizeX obstacle-size)
-      (q/rect x y box-sizeX obstacle-size))
+      (q/rect x (- y obstacle-sizeY obstacle-gap) obstacle-sizeX obstacle-sizeY)
+      (q/rect x y obstacle-sizeX obstacle-sizeY))
     ;(println "Value of a: " (:x m) ", Value of b: " (:y m))
     )
 
