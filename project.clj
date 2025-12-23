a2b7d2e9a7ac58d39aa4560afc70e1ca0a390f15
FrayedBlade <kestrel_o7@proton.me>
2025-12-23 09:37:29 -0800

    new commit2

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main ^:skip-aot game-project.core
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [quil "4.3.1323"]])
+                 [quil "4.3.1323"]]
+  :plugins [[cider/cider-nrepl "0.49.0"]])
--- src/game_project/core.clj
+++ src/game_project/core.clj
@@ -222,17 +222,20 @@
 
 
 
-(q/defsketch game-project
-             :title "Game"
-             :size [screen-sizeX screen-sizeY]
-             ; setup function called only once, during sketch initialization.
-             :setup setup
-             ; update-state is called on each iteration before draw-state.
-             :update update-state
-             :draw draw-state
-             :features [:keep-on-top]
-             ; This sketch uses functional-mode middleware.
-             ; Check quil wiki for more info about middlewares and particularly
-             ; fun-mode.
-             ;:middleware [m/fun-mode]
-             )
+(defn -main []
+  (q/defsketch game-project
+               :title "Game"
+               :size [screen-sizeX screen-sizeY]
+               ; setup function called only once, during sketch initialization.
+               :setup setup
+               ; update-state is called on each iteration before draw-state.
+               :update update-state
+               :draw draw-state
+               :features [:keep-on-top]
+               ; This sketch uses functional-mode middleware.
+               ; Check quil wiki for more info about middlewares and particularly
+               ; fun-mode.
+               ;:middleware [m/fun-mode]
+               ))
+
+
